e3240ee44893f2d8d15c38b8967a169c42c11e3e
Federico Alejo Marquez <17768182+fefu-marquez@users.noreply.github.com>
2024-01-19 08:38:32 -0300

    CI: Add fake env with firebase secret data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // Replace this with your firebase credentials
+  firebase: {
+    apiKey: 'your API key',
+    authDomain: 'your domain',
+    projectId: 'your project id',
+    storageBucket: 'your storage bucket',
+    messagingSenderId: 'your message sender id',
+    appId: 'your app id',
+  },
 };
